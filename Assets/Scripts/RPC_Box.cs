ddfb35235d0ed49903a630bb61f291278360d6a3
tinytaurenn <dumortier.matthieu@gmail.com>
2025-08-21 12:36:13 +0200

    learning

--- Assets/Scripts/RPC_Box.cs
+++ Assets/Scripts/RPC_Box.cs
@@ -1,13 +1,32 @@
 using PurrNet;
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RPC_Box : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] TextMeshPro m_SimpleText; 
+
+    public SyncVar<int> m_SimpleTextSync = new SyncVar<int>(ownerAuth : true);
+
+    private void Awake()
+    {
+        m_SimpleTextSync.onChanged += OnSimpleTextChanged; 
+    }
+
+    private void OnSimpleTextChanged(int obj)
+    {
+        m_SimpleText.text = obj.ToString();
+
+    }
+
     void Start()
     {
-        
+        m_SimpleTextSync.value = int.Parse(m_SimpleText.text);
+        //string to int method ? 
+
+
     }
 
     // Update is called once per frame
@@ -31,6 +50,24 @@ public class RPC_Box : NetworkBehaviour
             SetColor(Color.yellow);
         }
 
+        //sync var
+
+        if (Keyboard.current.numpad5Key.isPressed)
+        {
+            SetTextServer(1); //better on server objects
+        }
+        if (Keyboard.current.numpad6Key.isPressed)
+        {
+            //better for client objects
+            PlayerID? ownerID = m_SimpleTextSync.owner;
+            if(ownerID == null ) GiveOwnership(localPlayer); // simple to take ownership if the object is not owned
+
+
+            Debug.Log("owner id is" +  ownerID);
+            SetTargetText(ownerID.Value, -1); // if the item has ownership, just send to them
+
+        }
+
     }
     [ServerRpc(requireOwnership:false)]
     void SetColor(Color color)
@@ -46,8 +83,31 @@ public class RPC_Box : NetworkBehaviour
         //create instanced material for this object with material properties
         MaterialPropertyBlock mpb = new MaterialPropertyBlock();
         //GetComponent<Renderer>().GetPropertyBlock(mpb);
-
+        
         mpb.SetColor("_BaseColor", color);
         GetComponent<Renderer>().SetPropertyBlock(mpb);
+    }
+
+    [ServerRpc(requireOwnership:false)]
+    void SetTextServer(int text)
+    {
+        SetTextObserver(text);
+    }
+    //observers is not needed bc of syncvar
+    void SetTextObserver(int text)
+    {
+        m_SimpleTextSync.value += text;
+    }
+    [TargetRpc]
+    void SetTargetText(PlayerID target,int text)
+    {
+        m_SimpleTextSync.value += text;
+    }
+
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        m_SimpleTextSync.onChanged -= OnSimpleTextChanged; 
     }
 }
