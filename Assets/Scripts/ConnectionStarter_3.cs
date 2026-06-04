dd6c8c51fc93d061bc27f1745e2923b966373fc1
tinytaurenn <dumortier.matthieu@gmail.com>
2026-06-04 19:06:20 +0200

    new connection starter

--- Assets/Scripts/ConnectionStarter_3.cs
+++ Assets/Scripts/ConnectionStarter_3.cs
@@ -11,14 +11,13 @@ using Steamworks;
 public class ConnectionStarter_3 : MonoBehaviour
     {
         PurrTransport m_PurrTransport;
-    SteamTransport m_SteamTransport; 
-        UDPTransport m_UDPTransport; 
-        
+        SteamTransport m_SteamTransport; 
+        UDPTransport m_UDPTransport;
+        [SerializeField] private bool m_IsLocalPlaying = true; 
+        [SerializeField]string m_ServerAdress = "127.0.0.1";
+        [SerializeField]  ushort m_ServerPort = 5000; 
         private NetworkManager _networkManager;
-        [SerializeField] private LobbyDataHolder _lobbyDataHolder;
-
-
-        [SerializeField] bool m_IsFromLobby; 
+ 
 
         private void Awake()
         {
@@ -53,59 +52,28 @@ public class ConnectionStarter_3 : MonoBehaviour
             //start server if this is the host
             //_networkManager.StartServer();
 
-
-
-            //_networkManager.StartClient(); 
-        }
-
-        void StartFromLobby()
-        {
-            _networkManager.transport = m_SteamTransport; 
-            if (!_lobbyDataHolder)
+            _networkManager.startClientFlags = StartFlags.ClientBuild; 
+            if (m_IsLocalPlaying)
             {
-                PurrLogger.LogError($"Failed to start connection. {nameof(LobbyDataHolder)} is null!", this);
-                return;
+                _networkManager.startClientFlags = (StartFlags)(-1);
+                _networkManager.startServerFlags = StartFlags.ServerBuild | StartFlags.Editor; 
+                m_UDPTransport.address = "127.0.0.1";
+                m_UDPTransport.serverPort = 5000;
             }
-
-            if (!_lobbyDataHolder.CurrentLobby.IsValid)
+            else
             {
-                PurrLogger.LogError($"Failed to start connection. Lobby is invalid!", this);
-                return;
+                _networkManager.startClientFlags = (StartFlags)(-1);
+                _networkManager.startServerFlags = StartFlags.ServerBuild;
+                m_UDPTransport.address = m_ServerAdress;
+                m_UDPTransport.serverPort = m_ServerPort;
+                //doesnt change adress
             }
-            if(!ulong.TryParse(_lobbyDataHolder.CurrentLobby.LobbyId, out ulong lobbyId))
-        {
-                Debug.LogError($"Failed to start connection. Lobby ID is invalid!", this);
-                return;
-        }
-            var lobbyOwner =  SteamMatchmaking.GetLobbyOwner(new CSteamID(lobbyId));
-            if (!lobbyOwner.IsValid())
-            {
-            Debug.LogError($"error, Lobby Owner: {lobbyOwner}, Lobby ID: {lobbyId}", this);
-            }
+            
 
-            m_SteamTransport.address = lobbyOwner.ToString();
-
-            //#if UTP_LOBBYRELAY
-            //            else if(_networkManager.transport is UTPTransport) {
-            //                if(_lobbyDataHolder.CurrentLobby.IsOwner) {
-            //                    (_networkManager.transport as UTPTransport).InitializeRelayServer((Allocation)_lobbyDataHolder.CurrentLobby.ServerObject);
-            //                }
-            //                (_networkManager.transport as UTPTransport).InitializeRelayClient(_lobbyDataHolder.CurrentLobby.Properties["JoinCode"]);
-            //            }
-            //#else
-            //                //P2P Connection, receive IP/Port from server
-            //#endif
-
-            if (_lobbyDataHolder.CurrentLobby.IsOwner)
-                _networkManager.StartServer();
-            StartCoroutine(StartClient());
+            _networkManager.StartClient(); 
         }
 
-        private IEnumerator StartClient()
-        {
-            yield return new WaitForSeconds(1f);
-            _networkManager.StartClient();
-        }
+        
      
     }
 
