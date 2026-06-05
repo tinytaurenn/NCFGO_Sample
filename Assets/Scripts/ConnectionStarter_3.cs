6dd8e2958ba54f2b513ce84e9620980660dcbfa6
tinytaurenn <dumortier.matthieu@gmail.com>
2026-06-05 19:54:27 +0200

    connectionWindow

--- Assets/Scripts/ConnectionStarter_3.cs
+++ Assets/Scripts/ConnectionStarter_3.cs
@@ -6,6 +6,8 @@ using System.Collections;
 using PurrLobby;
 using PurrNet.Steam;
 using Steamworks;
+using TMPro;
+using WebSocketSharp;
 
 
 public class ConnectionStarter_3 : MonoBehaviour
@@ -17,6 +19,15 @@ public class ConnectionStarter_3 : MonoBehaviour
         [SerializeField]string m_ServerAdress = "127.0.0.1";
         [SerializeField]  ushort m_ServerPort = 5000; 
         private NetworkManager _networkManager;
+        
+        // UI Connection
+        
+        [SerializeField] TextMeshProUGUI m_ServerAdressText;
+        [SerializeField] TextMeshProUGUI m_ServerPortText;
+        [SerializeField] TMP_InputField m_ServerAdressInputField;
+        [SerializeField] TMP_InputField m_ServerPortInputField;
+        [SerializeField]  GameObject m_ConnectionUI; 
+         
  
 
         private void Awake()
@@ -41,7 +52,7 @@ public class ConnectionStarter_3 : MonoBehaviour
                 PurrLogger.LogError($"Failed to start connection. {nameof(NetworkManager)} is null!", this);
                 return;
             }
-            StartNormal(); 
+            //StartNormal(); 
 
             
 
@@ -59,18 +70,42 @@ public class ConnectionStarter_3 : MonoBehaviour
                 _networkManager.startServerFlags = StartFlags.ServerBuild | StartFlags.Editor; 
                 m_UDPTransport.address = "127.0.0.1";
                 m_UDPTransport.serverPort = 5000;
+                _networkManager.StartServer();
             }
             else
             {
                 _networkManager.startClientFlags = (StartFlags)(-1);
                 _networkManager.startServerFlags = StartFlags.ServerBuild;
-                m_UDPTransport.address = m_ServerAdress;
-                m_UDPTransport.serverPort = m_ServerPort;
+                //m_UDPTransport.address = m_ServerAdress;
+                //m_UDPTransport.serverPort = m_ServerPort;
                 //doesnt change adress
             }
             
 
             _networkManager.StartClient(); 
+            
+            m_ConnectionUI.SetActive(false);
+        }
+        
+        public void StartLocalConnectionButton()
+        {
+            m_IsLocalPlaying = true;
+            StartNormal();
+        }
+
+        public void StartServerButton()
+        {
+            if (m_ServerAdressInputField.text.IsNullOrEmpty() || m_ServerPortInputField.text.IsNullOrEmpty())
+            {
+                Debug.LogError("Server address or port is empty!");
+                return;
+            }
+            m_UDPTransport.address = m_ServerAdressInputField.text;
+            ushort serverPort =  ushort.Parse(m_ServerPortInputField.text);
+            Debug.Log("server port is " + serverPort);
+            m_UDPTransport.serverPort = serverPort;
+            m_IsLocalPlaying = false;
+            StartNormal();
         }
 
         
